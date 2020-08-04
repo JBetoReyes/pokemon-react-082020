a2e62e8875a2a54d0a4bb1f4a30fa09b7a0da44c
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-04 06:35:14 -0500

    adding webpack config

---
+++ webpack/webpack.base.config.js
@@ -0,0 +1,49 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const { resolve } = require("path");
+module.exports = (env) => {
+  const srcPath = resolve(__dirname, '..', 'src');
+  const appPath = resolve(srcPath, 'app');
+  const publicPath = resolve(srcPath, 'public');
+  console.log(resolve(appPath, 'components'))
+  return {
+    entry: resolve(srcPath, "app", "index.tsx"),
+    output: {
+      filename: "app-[hash:6].js",
+      path: resolve(srcPath, "dist"),
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+      alias: {
+        '@src': srcPath,
+        '@styles': resolve(publicPath, 'styles'),
+        '@components': resolve(appPath, 'components'),
+      },
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: resolve(publicPath, 'index.html'),
+        filename: 'index.html'
+      }),
+      new CleanWebpackPlugin({
+        verbose: true,
+        cleanOnceBeforeBuildPatterns: ['**/app*'],
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: {
+            loader: 'awesome-typescript-loader'
+          }
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          loader: 'file-loader',
+        }
+      ]
+    }
+  };
+};
---
+++ webpack/webpack.development.config.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const { resolve } = require('path');
+const baseConfig = require('./webpack.base.config');
+
+module.exports = (env) => ({
+  ...merge(baseConfig(env), {
+    mode: 'development',
+    module: {
+      rules: [{
+        test: /\.s?css$/,
+      loader: ['style-loader', 'css-loader', 'sass-loader']
+      }],
+    },
+    devServer: {
+      contentBase: resolve(__dirname, '..', 'dist'),
+      host: '0.0.0.0',
+      open: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'X-Requested-With, content-type, Authorization',
+      }
+    }
+  })
+});
