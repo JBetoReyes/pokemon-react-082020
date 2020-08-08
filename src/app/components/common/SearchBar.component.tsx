4f9fa816d37ed8e94565bedf8377a3b6d0acd0e3
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-08 17:38:32 -0500

    adding styles and handlers

--- src/app/components/common/SearchBar.component.tsx
+++ src/app/components/common/SearchBar.component.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState, Validator } from 'react';
+import PropTypes from 'prop-types';
 import './_SearchBar.component.scss';
 
 export type OwnProps = {
   title: string;
+  setPokemons: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-const SearchBar = ({ title }: OwnProps): JSX.Element => {
+type Props = {
+  propTypes: Record<string, Validator<any>> & OwnProps;
+};
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>;
+type SubmitEvent = React.FormEvent<HTMLFormElement>;
+
+const SearchBar = ({ title, setPokemons }: OwnProps): JSX.Element => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearchInput = (event: ChangeEvent) => {
+    if (event.target.value.trim().length > 2) {
+      setSearchValue(event.target.value.trim());
+    }
+  };
+
+  const handelSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+    setPokemons((pokemons: string[]) => {
+      return [...pokemons, searchValue];
+    });
+    setSearchValue('');
+  };
+
   return (
     <div className="search-bar__container">
       <h2 className="search-bar__title">{title}</h2>
-      <input className="search-bar__input" type="text" />
+      <form className="search-bar__form" onSubmit={handelSubmit}>
+        <input
+          className="search-bar__input"
+          type="text"
+          value={searchValue}
+          onChange={handleSearchInput}
+        />
+      </form>
     </div>
   );
+};
+
+SearchBar.propTypes = {
+  setPokemons: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
