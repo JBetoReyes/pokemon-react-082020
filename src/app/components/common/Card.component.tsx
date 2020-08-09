eb84989ed0f94c1ae2abdba9a015a76e86cd2f66
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-08 22:32:46 -0500

    the card component is receives the url dinamically

--- src/app/components/common/Card.component.tsx
+++ src/app/components/common/Card.component.tsx
@@ -4,21 +4,17 @@ import './_Card.component.scss';
 export type OwnProps = {
   title: string;
   subTitle: string;
+  url: string;
 };
 
 type Props = OwnProps & {
   children?: ReactNode;
 };
 
-const Card = ({ title, subTitle }: Props): JSX.Element => {
-  const url = process.env.POKEMON_IMAGES_URL;
+const Card = ({ title, subTitle, url }: Props): JSX.Element => {
   return (
     <div className="card-item">
-      <img
-        src={`${url}/25.png`}
-        alt="pokemonimage"
-        className="card-item__img"
-      />
+      <img src={`${url}`} alt="pokemonimage" className="card-item__img" />
       <div className="card-item__details">
         <p className="card-item__details--title">{title}</p>
         <p className="card-item__details--sub-title">{subTitle}</p>
