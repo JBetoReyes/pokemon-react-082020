75590068e9e7abfb8e1ae7799f6e552cd5fdbfd1
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-08 22:44:41 -0500

    adds the pokemon call

--- src/app/home/Home.component.tsx
+++ src/app/home/Home.component.tsx
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import SearchBar from '@components/common/SearchBar.component';
+import Carousel from '@components/common/Carousel.component';
+import Card from '@components/common/Card.component';
+
+export interface IPokemonAPI {
+  results: IPokemonAM[];
+}
+
+export interface IPokemonAM {
+  name: string;
+  url: string;
+}
+
+export interface IPokemon extends IPokemonAM {
+  number: number;
+}
 
 export default (): JSX.Element => {
-  return <div> Hello </div>;
+  const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const pokemonImagesUrl = process.env.POKEMON_IMAGES_URL;
+  const getPokemons = async () => {
+    const api = process.env.POKEMON_API;
+    const resp = await fetch(api as string);
+    const { results } = (await resp.json()) as IPokemonAPI;
+    const pokemonsMap = results.map<IPokemon>((pokemon: IPokemonAM) => {
+      const numberRegex = /^.*\/(\d\d?\d?\d?)\/?$/;
+      let number: number | string = 36;
+      if (numberRegex.test(pokemon.url)) {
+        [, number] = pokemon.url.match(numberRegex) as RegExpMatchArray;
+      }
+      return {
+        ...pokemon,
+        number: +number,
+      };
+    });
+    setPokemons(pokemonsMap);
+  };
+  useEffect(() => {
+    getPokemons();
+  }, []);
+  return (
+    <section>
+      <SearchBar title="Which is your favorite pokemon?" />
+      <Carousel>
+        {pokemons.map(({ name, number }) => (
+          <Card
+            key={`${number}-${name}`}
+            title={name}
+            subTitle={`${number}`}
+            url={`${pokemonImagesUrl}/${number}.png`}
+          />
+        ))}
+      </Carousel>
+    </section>
+  );
 };
