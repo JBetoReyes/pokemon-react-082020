e4d1f04aabdb16aab54d7c82ca6dc0265878bd4f
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-10 21:21:10 -0500

    ground work for pagination

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,7 +1,27 @@
-export const fetchPokemons = async (): Promise<IPokemonApp[]> => {
-  const apiUrl = process.env.POKEMON_API;
-  const rawResponse = await fetch(apiUrl as string);
-  const { results } = (await rawResponse.json()) as IPokemonResponse;
+export const fetchPokemons = async (page = 1): Promise<IPokemonApp[]> => {
+  const pokemonResponse = localStorage.getItem('pokemonResponse');
+  let results: IPokemonAM[] = [];
+  let next = '';
+  if (pokemonResponse) {
+    const storedResponse = JSON.parse(pokemonResponse) as IPokemonResponse;
+    results = [...results, ...storedResponse.results];
+    next = storedResponse.next as string;
+  }
+  if (page * 20 > results.length) {
+    const apiUrl = next || process.env.POKEMON_API;
+    const rawResponse = await fetch(apiUrl as string);
+    const newResponse = (await rawResponse.json()) as IPokemonResponse;
+    results = [...results, ...newResponse.results];
+    next = newResponse.next as string;
+  }
+  localStorage.setItem(
+    'pokemonResponse',
+    JSON.stringify({
+      results,
+      next,
+      loaded: results.length,
+    })
+  );
   const pokemons = results.map<IPokemonApp>(
     (pokemon: IPokemonAM): IPokemonApp => {
       const numberRegex = /^.*\/(\d{1,4})\/?$/;
@@ -19,6 +39,8 @@ export const fetchPokemons = async (): Promise<IPokemonApp[]> => {
 };
 
 export interface IPokemonResponse {
+  next: string;
+  previous: string;
   results: IPokemonAM[];
 }
 
