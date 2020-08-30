a8d0d56d2eb546238c5075cf7590cd02e56f78a4
beto <jose.a.reyes.quevedo@accenture.com>
2020-08-29 19:48:50 -0500

    fix Hero screen

--- src/app/components/heroApp/heroes/HeroScreen.tsx
+++ src/app/components/heroApp/heroes/HeroScreen.tsx
@@ -1,16 +1,27 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import React from 'react';
-import { useParams, Redirect } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { useParams, Redirect, RouteComponentProps } from 'react-router-dom';
+import { AppClickEvent } from '@typings/htmlEvents';
 import getHeroesById from '../services/getHeroesById';
 import { IHero } from '../data/heroes';
 
-const HeroScreen = (): JSX.Element => {
+type Props = RouteComponentProps;
+
+const HeroScreen = ({ history }: Props): JSX.Element => {
   const { heroId } = useParams<{ heroId: string }>();
-  const hero = getHeroesById(heroId);
-  console.log('hero', hero);
+  const hero = useMemo<IHero | undefined>(() => getHeroesById(heroId), [
+    heroId,
+  ]);
   if (!hero) {
     return <Redirect to="/" />;
   }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleClick = (e: AppClickEvent) => {
+    if (history.length <= 2) {
+      history.push('/');
+    }
+    history.goBack();
+  };
   const {
     superhero,
     publisher,
@@ -20,7 +31,7 @@ const HeroScreen = (): JSX.Element => {
   } = hero as IHero;
   return (
     <div className="row m-5">
-      <div className="col-4">
+      <div className="col-3">
         <img
           src={`../heroes/${heroId}.jpg`}
           alt={superhero}
@@ -50,7 +61,11 @@ const HeroScreen = (): JSX.Element => {
         <h5> Characters </h5>
         <p>{characters}</p>
 
-        <button type="button" className="btn btn-outline-info">
+        <button
+          type="button"
+          className="btn btn-outline-info"
+          onClick={handleClick}
+        >
           Return
         </button>
       </div>
