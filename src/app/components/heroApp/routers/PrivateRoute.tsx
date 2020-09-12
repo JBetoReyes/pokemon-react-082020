e31e20dd5efb893970cf31818f8f63ac15eab2a8
beto <jose.a.reyes.quevedo@accenture.com>
2020-09-12 11:25:41 -0500

    private route

---
+++ src/app/components/heroApp/routers/PrivateRoute.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  RouteProps,
+} from 'react-router-dom';
+
+export type OwnProps = {
+  isAuthenticated: boolean;
+  component: React.ComponentType<RouteComponentProps>;
+};
+
+export type Props = OwnProps & RouteProps;
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}: Props): JSX.Element => {
+  if (rest.location) {
+    localStorage.setItem('lastPath', rest.location.pathname);
+  }
+  const componentSwitch = (props: RouteComponentProps) => {
+    return isAuthenticated ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/login" />
+    );
+  };
+  return <Route {...rest} component={componentSwitch} />;
+};
