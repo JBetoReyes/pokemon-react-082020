6289eae595386b67357367338c9050928993a700
beto <josereyes55@hotmail.com>
2020-11-19 19:58:00 -0600

    running prettier

--- src/app/containers/App.component.tsx
+++ src/app/containers/App.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '@components/layout/Header.component';
-import Home from '../home/Home.component';
+import Home from '../pages/home/Home.component';
 
 const App = (): JSX.Element => {
   return (
