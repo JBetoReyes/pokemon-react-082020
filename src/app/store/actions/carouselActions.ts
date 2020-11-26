359866f8c5f0e4c5bd8a73e7f2f958dd7e6f26d1
beto <josereyes55@hotmail.com>
2020-11-25 18:09:31 -0600

    adding carousel actions

---
+++ src/app/store/actions/carouselActions.ts
@@ -0,0 +1,34 @@
+import { fetchPokemons } from '@services/pokemonService';
+import { ThunkAction } from 'redux-thunk';
+import { IPokemon } from '../../models/pokemonModel';
+import { IStoreState } from '../../models/storeModel';
+
+export const SET_POKEMONS = '[Carousel] Set pokemons';
+
+interface ISetMainCarousel {
+  type: typeof SET_POKEMONS;
+  payload: IPokemon[];
+}
+
+export type CarouselActionsType = ISetMainCarousel;
+
+const setMainCarousel = (pokemons: IPokemon[]): CarouselActionsType => ({
+  type: '[Carousel] Set pokemons',
+  payload: pokemons,
+});
+
+export const loadMainCarousel = (): ThunkAction<
+  void,
+  IStoreState,
+  void,
+  CarouselActionsType
+> => {
+  return async (dispatch) => {
+    try {
+      const pokemons = await fetchPokemons();
+      dispatch(setMainCarousel(pokemons));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
