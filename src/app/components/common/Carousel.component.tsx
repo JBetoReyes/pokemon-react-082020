e5833fe7932c3f5943fa7769aa1e922f2398eae5
beto <josereyes55@hotmail.com>
2020-11-25 21:56:20 -0600

    deletes comments

--- src/app/components/common/Carousel.component.tsx
+++ src/app/components/common/Carousel.component.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { LegacyRef, ReactNode, useEffect, useRef } from 'react';
 import './_Carousel.component.scss';
 
 type Props = {
@@ -6,6 +6,19 @@ type Props = {
 };
 
 const Carousel = ({ children }: Props): JSX.Element => {
+  // const carouselRef = useRef<HTMLDivElement | null>(null);
+  // useEffect(() => {
+  //   const options = {
+  //     root: null,
+  //     rootMargin: "0px",
+  //     threshold: .5
+  //   };
+  //   const observer = new IntersectionObserver((entities) => {
+  //     console.log('entities', entities);
+  //   }, options);
+  //   observer.observe(carouselRef.current as any)
+  //   return () => observer.unobserve(carouselRef.current as any);
+  // }, [carouselRef.current])
   return (
     <div className="carousel">
       <div className="carousel__container">{children}</div>
