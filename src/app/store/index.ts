c7b6d43dca1bca2b35d7d587b957f2e05b5a3fdc
beto <josereyes55@hotmail.com>
2020-11-26 20:02:45 -0600

    initilizing app with cache

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,6 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { IPokemon } from '../models/pokemonModel';
 
 import rootReducer from './reducers';
 
@@ -7,7 +8,16 @@ const composeEnhancers =
   (typeof window !== 'undefined' &&
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
+const pokemons: IPokemon[] = localStorage.getItem('pokemons')
+  ? JSON.parse(localStorage.getItem('pokemons') as string)
+  : [];
 export default createStore(
   rootReducer,
+  {
+    mainCarousel: {
+      isloading: true,
+      pokemons,
+    },
+  },
   composeEnhancers(applyMiddleware(thunk))
 );
