e7db45e067ea20b9740e3d9caa3d6fb71ede3f51
beto <josereyes55@hotmail.com>
2020-11-26 20:00:24 -0600

    adding loading points

---
+++ src/app/components/common/LoadingDots.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LoadingDots.scss';
+
+const LoadingDots = () => {
+  return (
+    <div className="overlay">
+      <ul className="loading-dots" >
+        <li />
+        <li />
+        <li />
+      </ul>
+    </div>
+  )
+}
+
+export default LoadingDots;
