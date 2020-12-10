e0ffa047a182dcfac8fe8a4c0349cdb4e9a5e23c
beto <josereyes55@hotmail.com>
2020-12-10 09:06:10 -0600

    refactors url

--- src/app/models/pokemonModel.ts
+++ src/app/models/pokemonModel.ts
@@ -1,17 +1,63 @@
 import { IAppFetchResponse } from '@typings/util/fetchWrapper';
 
-export interface IPokemonNM {
+export interface ILinkRecord {
   name: string;
   url: string;
 }
 
-export interface IPokemon extends IPokemonNM {
+export interface IStatsNM {
+  base_stat: number;
+  effort: number;
+  stat: ILinkRecord;
+}
+
+export interface IPokemonTypeNM {
+  slot: number;
+  type: ILinkRecord;
+}
+
+export interface IPokemonTypeRelationsResponse extends IAppFetchResponse {
+  damage_relations: {
+    double_damage_from: ILinkRecord[];
+    double_damage_to: ILinkRecord[];
+    half_damage_from: ILinkRecord[];
+    half_damage_to: ILinkRecord[];
+    no_damage_from: ILinkRecord[];
+    no_damage_to: ILinkRecord[];
+  };
+  pokemon: {
+    pokemon: ILinkRecord;
+  }[];
+}
+
+export interface IStats {
+  name: string;
+  baseStat: number;
+}
+
+export interface IPokemonType {
+  name: string;
+  url: string;
+}
+
+export interface IPokemonDetailsResponse extends IAppFetchResponse {
+  stats: IStatsNM[];
+  types: IPokemonTypeNM[];
+}
+
+export interface IPokemon extends ILinkRecord {
   number: string;
+}
+
+export interface IPokemonDetails {
+  stats: IStats[];
+  types: IPokemonType[];
+  weaknesses: string[];
 }
 
 export interface IPokemonResponse extends IAppFetchResponse {
   count: number;
   next: string;
   previous: string;
-  results: IPokemonNM[];
+  results: ILinkRecord[];
 }
--- src/app/services/pokemonService.ts
+++ src/app/services/pokemonService.ts
@@ -1,18 +1,27 @@
-import { IPokemon, IPokemonNM, IPokemonResponse } from '../models/pokemonModel';
+import {
+  IPokemon,
+  IPokemonDetails,
+  IPokemonDetailsResponse,
+  ILinkRecord,
+  IPokemonResponse,
+  IPokemonType,
+  IStats,
+  IPokemonTypeRelationsResponse,
+} from '../models/pokemonModel';
 import { get } from '../util/fetchWrapper';
 import * as mockResponse from '../store/mockResponse.json';
 
 const numberRegex = /^.*\/(\d{1,4})\/?$/;
+const apiUrl = process.env.POKEMON_API as string;
 
 export const fetchPokemons = async (page: number) => {
-  const apiUrl = process.env.POKEMON_API as string;
   const storePage = localStorage.getItem('page');
   if (storePage && +storePage >= page) {
     return;
   }
   const offset = page === 1 ? 0 : (page - 1) * 20;
   const response = await get<IPokemonResponse>(
-    `${apiUrl}?offset=${offset}&limit=20`
+    `${apiUrl}/pokemon?offset=${offset}&limit=20`
   );
 
   const pokemons = response.results.filter(filterResponse).map(processResponse);
@@ -20,15 +29,57 @@ export const fetchPokemons = async (page: number) => {
   return pokemons;
 };
 
+export const fetchPokemon = async (
+  pokemonNumber: string
+): Promise<IPokemonDetails> => {
+  const response = await get<IPokemonDetailsResponse>(
+    `${apiUrl}/pokemon/${pokemonNumber}`
+  );
+  const typesRelations = await Promise.all(
+    response.types.map((type) => fetchPokemonTypeRelations(type.type.name))
+  );
+  return {
+    types: response.types.map<IPokemonType>((type) => type.type),
+    stats: response.stats.map<IStats>((stat) => {
+      const { name } = stat.stat;
+      return {
+        name: `${name.charAt(0).toUpperCase()}${name
+          .substr(1)
+          .replace('-', ' ')}`,
+        baseStat: stat.base_stat,
+      };
+    }),
+    weaknesses: typesRelations.reduce<string[]>(
+      (acc, curr) => [...acc, ...curr.weak],
+      []
+    ),
+  };
+};
+
+export const fetchPokemonTypeRelations = async (type: string) => {
+  const response = await get<IPokemonTypeRelationsResponse>(
+    `${apiUrl}/type/${type}`
+  );
+  return {
+    type,
+    weak: response.damage_relations.double_damage_from.map(
+      (relation) => relation.name
+    ),
+    strong: response.damage_relations.double_damage_to.map(
+      (relation) => relation.name
+    ),
+  };
+};
+
 export const fetchAllPokemons = () => {
   return mockResponse.results.filter(filterResponse).map(processResponse);
 };
 
-export const filterResponse = (pokemon: IPokemonNM) => {
+export const filterResponse = (pokemon: ILinkRecord) => {
   return numberRegex.test(pokemon.url);
 };
 
-export const processResponse = (pokemon: IPokemonNM): IPokemon => {
+export const processResponse = (pokemon: ILinkRecord): IPokemon => {
   const [, number] = pokemon.url.match(numberRegex) as RegExpMatchArray;
   return {
     ...pokemon,
